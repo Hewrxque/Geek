c7a769ddb586d70cae39603d636f411eee608c1d
Edenilson Souza <edenilson.sza@gmail.com>
2022-11-18 17:40:25 -0300

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,25 +7,40 @@ export default function Home() {
         <div className="container">
             <div className="container-Home">
                 <div className="wrap-Home">
-                    <form className="Home-form">
-                        <span className="Home-form-title">
+                    <form className="Home-form" style={{ height: '100%', width: '100%' }}>
+                        <span
+                            className="Home-form-title"
+                            style={{ display: 'flex', position: 'absolute', height: '80px' }}
+                        >
                             <img src={IMG} alt="Geek" />
                         </span>
 
-                        <div className="container-Home-form-btn">
-                            <button className="Home-form-btn">Eventos</button>
-                        </div>
-                        <div className="container-Home-form-btn">
-                            <button className="Home-form-btn">Feed</button>
-                        </div>
-                        <div className="container-Home-form-btn">
-                            <button className="Home-form-btn">Menu</button>
-                        </div>
-                        <div className="container-Home-form-btn">
-                            <button className="Home-form-btn">Perfil</button>
-                        </div>
-                        <div className="container-Home-form-btn">
-                            <button className="Home-form-btn">Sair</button>
+                        <div
+                            style={{
+                                display: 'flex',
+                                width: '100%',
+                                height: '100%',
+                                whiteSpace: 'normal',
+                                flexFlow: 'row wrap',
+                                flexWrap: 'wrap',   
+                                alignItems: 'center'
+                            }}
+                        >
+                            <div className="container-Home-form-btn" style={{ padding: '5px' }}>
+                                <button className="Home-form-btn">Eventos</button>
+                            </div>
+                            <div className="container-Home-form-btn" style={{ padding: '5px' }}>
+                                <button className="Home-form-btn">Feed</button>
+                            </div>
+                            <div className="container-Home-form-btn" style={{ padding: '5px' }}>
+                                <button className="Home-form-btn">Menu</button>
+                            </div>
+                            <div className="container-Home-form-btn" style={{ padding: '5px' }}>
+                                <button className="Home-form-btn">Perfil</button>
+                            </div>
+                            <div className="container-Home-form-btn" style={{ padding: '5px' }}>
+                                <button className="Home-form-btn">Sair</button>
+                            </div>
                         </div>
                     </form>
                 </div>
